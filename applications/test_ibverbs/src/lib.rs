5c4898b02ba16f8d03fa6abe94bebefd4e71be92
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2024-07-19 17:46:46 +0200

    test_ibverbs: Open the device

--- applications/test_ibverbs/src/lib.rs
+++ applications/test_ibverbs/src/lib.rs
@@ -5,10 +5,12 @@ extern crate alloc;
 use alloc::{string::String, vec::Vec};
 
 pub fn main(_args: Vec<String>) -> isize {
-    let device = ibverbs::devices()
+    let context = ibverbs::devices()
         .expect("failed to list devices")
         .iter()
         .next()
-        .expect("failed to get device");
+        .expect("failed to get device")
+        .open()
+        .expect("failed to open device");
     0
 }
